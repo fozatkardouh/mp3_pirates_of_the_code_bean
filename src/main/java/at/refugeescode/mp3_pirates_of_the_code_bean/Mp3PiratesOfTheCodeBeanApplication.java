044af9328585c1080617e565b7c549e98d0ab7cf
Fozat Kardouh <fozatkardouh@gmail.com>
2018-03-22 10:07:11 +0100

    added project

---
+++ src/main/java/at/refugeescode/mp3_pirates_of_the_code_bean/Mp3PiratesOfTheCodeBeanApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.mp3_pirates_of_the_code_bean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp3PiratesOfTheCodeBeanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp3PiratesOfTheCodeBeanApplication.class, args);
+	}
+}
---
+++ src/test/java/at/refugeescode/mp3_pirates_of_the_code_bean/Mp3PiratesOfTheCodeBeanApplicationTests.java
@@ -0,0 +1,16 @@
+package at.refugeescode.mp3_pirates_of_the_code_bean;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Mp3PiratesOfTheCodeBeanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
